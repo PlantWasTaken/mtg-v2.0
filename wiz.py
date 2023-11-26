e60b314c86b1802d70d0583edc308eb535fb74eb
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2023-11-26 11:13:34 +0100

    Update wiz.py

--- wiz.py
+++ wiz.py
@@ -1,8 +1,9 @@
 import pyautogui
 import time as t
+import asyncio
 
 import json
-import psutil
+import psutil 
 import random
 
 from salted import get_bin_salt
@@ -11,129 +12,139 @@ from wizreq import open_register_site
 #ORDER
 #open website, tab 3 times
 #make account
-def create_account(usn):
-    process_id = open_register_site(url='https://myaccounts.wizards.com/register')    
+async def create_account(usn, lock):
+    lock = lock
 
-    def gen_pass():
-        ps = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        ps = [i for i in ps]
-        random.shuffle(ps)
-        return "".join(ps[0:10])
+    async with lock:
+        print("wiz")
+        process_id = await open_register_site(url='https://myaccounts.wizards.com/register', lock=lock)    
 
-    t.sleep(3)
+        async def gen_pass():
+            ps = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+            ps = [i for i in ps]
+            random.shuffle(ps)
+            return "".join(ps[0:10])
 
-    with open("accountdatabase.json", "r") as read_file:
-        data = json.load(read_file)
+        await asyncio.sleep(3)
 
-    seed = data['salt_seed'] #salt seed
-    data['salt_seed'] = int(data['salt_seed'])+1
+        with open("accountdatabase.json", "r") as read_file:
+            data = json.load(read_file)
 
-    with open("accountdatabase.json", "w") as write_file:
-        json.dump(data, write_file, indent=4)
+        seed = data['salt_seed'] #salt seed
+        data['salt_seed'] = int(data['salt_seed'])+1
+
+        with open("accountdatabase.json", "w") as write_file:
+            json.dump(data, write_file, indent=4)
 
 
-    mail = 'meowkingkitten314'
+        mail = 'meowkingkitten314'
+        
+        mail2 = 'gmail.com'
 
-    mail2 = 'gmail.com'
+        passw = 'CoolCat123'
+        passw = await gen_pass()
 
-    passw = 'CoolCat123'
-    passw = gen_pass()
+        #usn = 'Cool cat'
+        usn = usn
 
-    #usn = 'Cool cat'
-    usn = usn
+        print("get bin salt")
+        mail_salt = await get_bin_salt(seed=seed, mail=mail, lock=lock)
 
-    mail_salt = get_bin_salt(seed=seed, mail=mail)
-
-    mail_salt='me.owkingkitten314'
-    print(mail_salt)
-
-    t.sleep(0.7)    
-    pyautogui.press('tab') 
-    t.sleep(0.3)
-    pyautogui.press('tab') 
-    t.sleep(0.3)
-    pyautogui.press('tab') 
-
-    pyautogui.write("plant")
-    pyautogui.press('tab') 
-    pyautogui.write("egg")
-    pyautogui.press('tab') 
-    for i in range(13):
-        pyautogui.write("n")
-    pyautogui.press('tab') 
-    for i in range(2):
-        pyautogui.write("j")
-    pyautogui.press('tab') 
-    pyautogui.write("2")
-    pyautogui.press('tab') 
-    pyautogui.write("1")
-    for i in range(2):
+        await asyncio.sleep(0.7)   
         pyautogui.press('tab') 
-        pyautogui.write(mail_salt)
-        pyautogui.hotkey('altright','2')
+        await asyncio.sleep(0.3)
+        pyautogui.press('tab') 
+        await asyncio.sleep(0.3)
+        pyautogui.press('tab') 
 
+        pyautogui.write("plant")
+        pyautogui.press('tab') 
+        pyautogui.write("egg")
+        pyautogui.press('tab') 
+
+        for i in range(13):
+            pyautogui.write("n")
+
+        pyautogui.press('tab') 
+
+        for i in range(2):
+            pyautogui.write("j")
+
+        pyautogui.press('tab') 
+        pyautogui.write("2")
+        pyautogui.press('tab') 
+        pyautogui.write("1")
+
+        for i in range(2):
+            pyautogui.press('tab') 
+            pyautogui.write(mail_salt)
+            pyautogui.hotkey('altright','2')
+            pyautogui.write(mail2)
+
+        pyautogui.press('tab') 
+        pyautogui.write(usn)
+        for i in range(2):
+            pyautogui.press('tab') 
+            pyautogui.write(passw)
+
+        for i in range(6):
+            pyautogui.press('tab') 
+
+        pyautogui.press('enter') 
+
+        await asyncio.sleep(2) #terminate browser
+        print(process_id)
+        process = psutil.Process(process_id)
+        process.terminate()
+        #account has been created.
+        #awaiting email confirmation
+
+        from reademail import get_verification_link #get link, open link, redeem codes.
+        await asyncio.sleep(20) #wait for email to come through
+
+        verification_link = await get_verification_link(lock=lock)
+        print(verification_link)
+
+        process_id = await open_register_site(url=verification_link, lock=lock)  #activate account
+
+        await asyncio.sleep(5) #login wait period
+
+        #account login and code redemption
+        for i in range(3):
+            pyautogui.press('tab') 
+
+        pyautogui.write(mail_salt) #username
+        pyautogui.hotkey('altright','2')
         pyautogui.write(mail2)
 
-    pyautogui.press('tab') 
-    pyautogui.write(usn)
-    for i in range(2):
-        pyautogui.press('tab') 
+        pyautogui.press('tab') #password
+
         pyautogui.write(passw)
 
-    for i in range(6):
-        pyautogui.press('tab') 
+        for i in range(2):
+            pyautogui.press('tab') 
 
-    pyautogui.press('enter') 
+        #3V5l6PhNUK m.e.owkingkitten314
+        pyautogui.press('enter')    #login
 
-    t.sleep(2) #terminate browser
-    print(process_id)
-    process = psutil.Process(process_id)
-    process.terminate()
-    #account has been created.
-    #awaiting email confirmation
+        await asyncio.sleep(2)
 
-    from reademail import get_verification_link #get link, open link, redeem codes.
-    t.sleep(20) #wait for email to come through
+        for i in range(2):
+            pyautogui.press('tab') 
 
-    verification_link = get_verification_link()
-    print(verification_link)
+        #run code redemption
+        from codes import redeem_code
 
-    process_id = open_register_site(url=verification_link)  #activate account
+        await redeem_code(lock=lock) #code redeems
 
-    t.sleep(5)
+        process = psutil.Process(process_id)
+        process.terminate()
+        #terminate process.
 
-    #account login and code redemption
-    for i in range(3):
-        pyautogui.press('tab') 
-
-    pyautogui.write(mail_salt) #username
-    pyautogui.hotkey('altright','2')
-    pyautogui.write(mail2)
-
-    pyautogui.press('tab') #password
-
-    pyautogui.write(passw)
-
-    for i in range(2):
-        pyautogui.press('tab') 
-
-    #3V5l6PhNUK m.e.owkingkitten314
-    pyautogui.press('enter')    #login
-    t.sleep(2)
-
-    for i in range(2):
-        pyautogui.press('tab') 
-
-    #run code redemption
-    from codes import redeem_code
-
-    redeem_code() #code redeems
-    process = psutil.Process(process_id)
-    process.terminate()
-    #terminate process.
-
+    await asyncio.sleep(3)
     return [f'{mail_salt}@{mail2}', passw, usn]
 
+#create_account("cat")
 #exit()
 #f = open('accounts.txt', 'a')
 #
