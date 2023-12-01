26871bc65df63b28230e2c98fc42ac30633f8362
PlantWasTaken <69759382+PlantWasTaken@users.noreply.github.com>
2023-12-01 20:48:09 +0100

    Update magicbot.py

--- magicbot.py
+++ magicbot.py
@@ -1,3 +1,5 @@
+#discord,async,psutil,pyautogui,email,imaplib
+
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -14,7 +16,7 @@ bot = commands.Bot(command_prefix='$',intents = discord.Intents.all())
 MY_GUILD = discord.Object(id=1177735119738519552)
 
 async def get_decklists_emails():
-    with open("accountdatabase.json", "r") as read_file:
+    with open(r'C:\Users\Max\Desktop\mtg account v2\mtg-v2.0-main\accountdatabase.json', "r") as read_file:
         data = json.load(read_file)
 
     decklists = [i for i in data['accounts']]
@@ -23,7 +25,7 @@ async def get_decklists_emails():
     return decklists,emails
 
 async def read_json():
-    with open("accountdatabase.json", "r") as read_file:
+    with open(r'C:\Users\Max\Desktop\mtg account v2\mtg-v2.0-main\accountdatabase.json', "r") as read_file:
         data = json.load(read_file)
 
     return data
@@ -124,6 +126,10 @@ async def new_account(interaction: discord.Interaction):
         try:
             mail, passw, usn = await create_account("Cool Cat", lock=asyncio.Lock()) #redudant lock will fix later
             print(mail,passw,usn)
+            data = await read_json()
+            seed = data['salt_seed']
+            print(seed)
+            print("\n\n")
             await interaction.edit_original_response(content=f'{str("<@" + str(sender_id) + ">")} Your account has been created\nAccount name: {usn}\nMail: {mail}\nPass: {passw}')
         except:
             await interaction.edit_original_response(content=f'please try again')
@@ -139,6 +145,17 @@ async def new_account(interaction: discord.Interaction):
     
     data = await read_json()
     await interaction.response.send_message(f'{data}')
+
+@bot.tree.command(name = "seed", guild=MY_GUILD, description="returns salt seed")
+async def new_account(interaction: discord.Interaction):
+    sender_id = interaction.user.id
+    if(sender_id not in valid_user_id):
+        await interaction.response.send_message(f'Not permitted',ephemeral=True)
+        raise ValueError
     
+    data = await read_json()
+    seed = data['salt_seed']
+    await interaction.response.send_message(f'{seed}\n{bin(seed)}')
+
 if __name__ == '__main__':
     bot.run(token)
